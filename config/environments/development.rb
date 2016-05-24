7f3a7f3b63e683a5779cfe2f56e433999313189a
Teenu <teenu@carmatec.com>
2016-05-24 18:31:42 +0530

    Configuration filr

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,12 @@ Rails.application.configure do
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
